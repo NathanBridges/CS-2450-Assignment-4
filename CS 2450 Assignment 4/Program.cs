79df177805f247ef97be5041d9f6f15b91c27ec5
Nathan <Darcendsun@gmail.com>
2014-06-02 00:49:29 -0600

    original

--- CS 2450 Assignment 4/Program.cs
+++ CS 2450 Assignment 4/Program.cs
@@ -18,8 +18,6 @@ static class Program
     {
         Console.WriteLine("Hello World!");
 
-        Console.WriteLine("Making a change!");
-
         Console.ReadLine();
     }//End Main()
 }//End class Program
