19c4a6d160a3357a955b7383b104674c3fa44dd3
Nathan <Darcendsun@gmail.com>
2014-06-02 00:39:46 -0600

    I hope this works...

---
+++ CS 2450 Assignment 4/Program.cs
@@ -0,0 +1,23 @@
+﻿// File Prolog
+// Name: Nathan Bridges
+// CS 2450
+// Project: Assignment 4
+// Date: 5/27/2014
+// 
+// I declare that the following code was written by me or provided 
+// by the instructor for this project. I understand that copying source
+// code from any other source constitutes cheating, and that I will receive
+// a zero on this project if I am found in violation of this policy.
+// ---------------------------------------------------------------------------
+
+using System;
+
+static class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World!");
+
+        Console.ReadLine();
+    }//End Main()
+}//End class Program
